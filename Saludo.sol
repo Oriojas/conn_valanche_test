cb8de884004e5afda38163e0faaa56f5a6921709
Oriojas <oscarriojas@gmail.com>
2024-10-07 18:17:55 -0500

    Creacion de contrato saludo

---
+++ Saludo.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.24;
+
+contract Saludo {
+
+    string saludo = "Saludo incial desde REMIX";
+
+    function leerSaludo() public view returns (string memory) {
+        return saludo;
+    }
+
+    function guardarSaludo(string memory _nuevoSaludo) public {
+        saludo = _nuevoSaludo;
+    }
+
+}
